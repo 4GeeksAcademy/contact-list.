33e88c749b1345073a8a86d830038720fdac8776
Cesar Contreras <cesar.alberto2003@outlook.com>
2024-11-28 19:14:38 +0000

    navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,14 +3,17 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+		<nav className="navbar navbar-expand-lg bg-body-tertiary">
+			<div className="container-fluid">
+				<Link className="navbar-brand text-black text-decoration-none" to="/">Contact List Sharguidev</Link>
+				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+					<div className="navbar-nav ms-auto">
+						<div className="btn btn-success text-white"><Link to="/createContactForm" className="text-white text-decoration-none">Add new contact</Link></div>
+					</div>
+				</div>
 			</div>
 		</nav>
 	);
